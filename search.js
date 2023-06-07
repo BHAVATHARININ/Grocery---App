1fe8b548810626b25682fd170ce4ec7c2e51321e
BHAVATHARININ <106963069+BHAVATHARININ@users.noreply.github.com>
2023-06-07 11:41:58 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,472 @@
+let products = {
+    data: [
+      {
+        productName: "Nescafe classic container",
+        category: "Beaverages",
+        price: "185",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/nescafee.jpg",
+      },
+      {
+        productName: "Sunrise",
+        category: "Beaverages",
+        price: "215",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/sunrise.jpg",
+      },
+      {
+        productName: "Continental Xtra",
+        category: "Beaverages",
+        price: "309",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/continental.jpg",
+      },
+      {
+        productName: "Rasna",
+        category: "Beaverages",
+        price: "55",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/rasna.jpg",
+      },
+      {
+        productName: "Taj Mahal",
+        category: "Beaverages",
+        price: "275",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/taj1.jpg",
+      },
+      {
+        productName: "3 Roses",
+        category: "Beaverages",
+        price: "170",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/3roses.jpg",
+      },
+      {
+        productName: "AVT Gold cup",
+        category: "Beaverages",
+        price: "115",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/avt.jpg",
+      },
+      {
+        productName: "Levista Instant Coffee",
+        category: "Beaverages",
+        price: "285",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/levista.jpg",
+      },
+      {
+        productName: "Horlicks",
+        category: "Beaverages",
+        price: "215",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/horlicks.jpg",
+      },
+      {
+        productName: "Boost",
+        category: "Beaverages",
+        price: "247",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/boost.jpg",
+      },
+      {
+        productName: "Complan",
+        category: "Beaverages",
+        price: "205",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/complan.jpg",
+      },
+      {
+        productName: "Pediasure",
+        category: "Beaverages",
+        price: "290",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/pediasure.jpg",
+      },
+      {
+        productName: "Tang",
+        category: "Beaverages",
+        price: "69",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/tang.jpg",
+      },
+      {
+        productName: "Cavin's Milkshake",
+        category: "Beaverages",
+        price: "45",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cavins.jpg",
+      },
+      {
+        productName: "Amul Kool",
+        category: "Beaverages",
+        price: "85",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/amul.jpg",
+      },
+      {
+        productName: "Badam",
+        category: "DryFruits",
+        price: "1000",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/badam.jpg",
+      },
+      {
+        productName: "Pista",
+        category: "DryFruits",
+        price: "790",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/pista.jpg",
+      },
+      {
+        productName: "Cashew",
+        category: "DryFruits",
+        price: "900",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cashew.jpg",
+      },
+      {
+        productName: "Raisin",
+        category: "DryFruits",
+        price: "300",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/raisin.jpg",
+      },
+      {
+        productName: "Walnut",
+        category: "DryFruits",
+        price: "275",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/walnut.jpg",
+      },
+      {
+        productName: "Dates",
+        category: "DryFruits",
+        price: "260",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/dates.jpg",
+      },
+      {
+        productName: "Biscuit",
+        category: "Snacks",
+        price: "60",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/biscuit.jpg",
+      },
+      {
+        productName: "Cake",
+        category: "Snacks",
+        price: "120",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cake.jpg",
+      },
+      {
+        productName: "Murukku",
+        category: "Snacks",
+        price: "90",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/murukku.jpg",
+      },
+      {
+        productName: "Popcorn",
+        category: "Snacks",
+        price: "70",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/popcorn.jpg",
+      },
+      {
+        productName: "Lays",
+        category: "Snacks",
+        price: "20",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/lays.jpg",
+      },
+      {
+        productName: "Bingo",
+        category: "Snacks",
+        price: "20",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/bingo.jpg",
+      },
+      {
+        productName: "Dairy Milk",
+        category: "Chocolates",
+        price: "175",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/dairymilk.jpg",
+      },
+      {
+        productName: "KitKat",
+        category: "Chocolates",
+        price: "80",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/kitkat.jpg",
+      },
+      {
+        productName: "Gems",
+        category: "Chocolates",
+        price: "40",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/gems.jpg",
+      },
+      {
+        productName: "Milky Bar",
+        category: "Chocolates",
+        price: "70",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/milkybar.jpg",
+      },
+      {
+        productName: "Belgian White",
+        category: "Chocolates",
+        price: "275",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/belgian.jpg",
+      },
+      {
+        productName: "Dark Chocolate",
+        category: "Chocolates",
+        price: "400",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/dark.jpg",
+      },
+      {
+        productName: "Blue Berry",
+        category: "FrozenFoods",
+        price: "300",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/blueberry.jpg",
+      },
+      {
+        productName: "Strawberry",
+        category: "FrozenFoods",
+        price: "600",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/strawberry.jpg",
+      },
+      {
+        productName: "Apricot",
+        category: "FrozenFoods",
+        price: "580",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/apricot.jpg",
+      },
+      {
+        productName: "Spinach",
+        category: "FrozenFoods",
+        price: "200",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/spinach.jpg",
+      },
+      {
+        productName: "Chicken",
+        category: "FrozenFoods",
+        price: "250",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/chicken.jpg",
+      },
+      {
+        productName: "Fish",
+        category: "FrozenFoods",
+        price: "400",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/fish.jpg",
+      },
+      {
+        productName: "Sensodyne",
+        category: "ToiletryProducts",
+        price: "120",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/sensodyne.jpg",
+      },
+      {
+        productName: "Brush",
+        category: "ToiletryProducts",
+        price: "70",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/brush.jpg",
+      },
+      {
+        productName: "Sunsilk",
+        category: "ToiletryProducts",
+        price: "230",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/sunsilk.jpg",
+      },
+      {
+        productName: "Surfexcel",
+        category: "ToiletryProducts",
+        price: "270",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/surfexcel.jpg",
+      },
+      {
+        productName: "Dove",
+        category: "ToiletryProducts",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/dove.jpg",
+      },
+      {
+        productName: "Harpic",
+        category: "ToiletryProducts",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/harpic.jpg",
+      },
+      {
+        productName: "Turmeric",
+        category: "Spices",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/turmeric.jpg",
+      },
+      {
+        productName: "Cinnamon",
+        category: "Spices",
+        price: "120",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cinnamon.jpg",
+      },
+      {
+        productName: "Cumin",
+        category: "Spices",
+        price: "270",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cumin.jpg",
+      },
+      {
+        productName: "Coriander",
+        category: "Spices",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/coriander.jpg",
+      },
+      {
+        productName: "Garlic",
+        category: "Spices",
+        price: "230",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/garlic.jpg",
+      },
+      {
+        productName: "Ginger",
+        category: "Spices",
+        price: "70",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/ginger.jpg",
+      },
+      {
+        productName: "Green Gram",
+        category: "Staples",
+        price: "120",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/greengram.jpg",
+      },
+      {
+        productName: "Maize",
+        category: "Staples",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/maize.jpg",
+      },
+      {
+        productName: "Soya Beans",
+        category: "Staples",
+        price: "270",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/soyabeans.jpg",
+      },
+      {
+        productName: "Rice",
+        category: "Staples",
+        price: "1200",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/rice.jpg",
+      },
+      {
+        productName: "Wheatr",
+        category: "Staples",
+        price: "290",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/wheat.jpg",
+      },
+      {
+        productName: "Beans",
+        category: "Staples",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/beans.jpg",
+      },
+      {
+        productName: "Juicer",
+        category: "KitchenAppliances",
+        price: "790",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/juicer.jpg",
+      },
+      {
+        productName: "Spoon Set",
+        category: "KitchenAppliances",
+        price: "140",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/spoonset.jpg",
+      },
+      {
+        productName: "Microwave Oven",
+        category: "KitchenAppliances",
+        price: "2700",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/oven.jpg",
+      },
+      {
+        productName: "Spatula",
+        category: "KitchenAppliances",
+        price: "230",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/spatula.jpg",
+      },
+      {
+        productName: "Toaster",
+        category: "KitchenAppliances",
+        price: "700",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/toaster.jpg",
+      },
+      {
+        productName: "Cooker",
+        category: "KitchenAppliances",
+        price: "1400",
+        image: "C:/Users/91790/OneDrive/Desktop/BHAVATHARINI NATESAN/WT/project/cooker.jpg",
+      },
+      
+      
+    ],
+  };
+  
+  for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "Rs. " + i.price;
+    container.appendChild(price);
+  
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
